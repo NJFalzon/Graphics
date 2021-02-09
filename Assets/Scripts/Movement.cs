ca95a2b93d2b5268d206844155d936ce43c8a38d
NJFalzon <nathan.falzon.c10326@mcast.edu.mt>
2021-02-09 11:58:01 +0100

    Finished Task 3

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] KeyCode forward;
+    [SerializeField] KeyCode forwardAlternate;
+    [Space]
+    [SerializeField] KeyCode backward;
+    [SerializeField] KeyCode backwardAlternate;
+    [Space]
+    [SerializeField] KeyCode left;
+    [SerializeField] KeyCode leftAlternate;
+    [Space]
+    [SerializeField] KeyCode right;
+    [SerializeField] KeyCode rightAlternate;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Look();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void MoveForward()
+    {
+        if (Input.GetKey(forward) || Input.GetKey(forwardAlternate))
+        {
+            rb.AddForce(transform.forward * 5, ForceMode.Impulse);
+        }
+    }
+
+    void MoveBackward()
+    {
+        if (Input.GetKey(backward) || Input.GetKey(backwardAlternate))
+        {
+            rb.AddForce(-transform.forward * 5, ForceMode.Impulse);
+        }
+    }
+
+    void MoveRight()
+    {
+        if (Input.GetKey(right) || Input.GetKey(rightAlternate))
+        {
+            rb.AddForce(transform.right * 5, ForceMode.Impulse);
+        }
+    }
+
+    void MoveLeft()
+    {
+        if (Input.GetKey(left) || Input.GetKey(leftAlternate))
+        {
+            rb.AddForce(-transform.right * 5, ForceMode.Impulse);
+        }
+    }
+
+
+    void Look()
+    {
+        float x = Input.GetAxis("Mouse X") * 2;
+
+        transform.Rotate(new Vector3(0, x, 0));
+    }
+
+    void Move()
+    {
+        MoveForward();
+        MoveBackward();
+        MoveLeft();
+        MoveRight();
+        LimitVelocity();
+    }
+
+    void LimitVelocity()
+    {
+        if(rb.velocity.magnitude > 5)
+        {
+            rb.velocity = rb.velocity.normalized*5;
+        }
+    }
+}
--- Assets/Scripts/RoadMaker.cs
+++ Assets/Scripts/RoadMaker.cs
@@ -1,65 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 [RequireComponent(typeof(MeshCollider))]
 public class RoadMaker : MonoBehaviour
 {
-    [SerializeField]
-    private float radius = 30f; // this defines the radius of the path
+    [Header("Dimensions of Road")]
 
-    [SerializeField]
-    private float segments = 300f;
+    [SerializeField] float radius = 30f;
+    [SerializeField] float segments = 300f;
+    [SerializeField] float lineWidth = 0.3f;
+    [SerializeField] float roadWidth = 8f;
+    [SerializeField] float edgeWidth = 1f;
+    [SerializeField] float edgeHeight = 1f;
 
-    [SerializeField]
-    private float lineWidth = 0.3f; // middle white line road marker
+    [Header("Randomness of Road"),Space]
 
-    [SerializeField]
-    private float roadWidth = 8f; // width of the road on each side of the line
+    [SerializeField] float wavyness = 5f;
+    [SerializeField] float waveScale = 0.1f;
+    [SerializeField] Vector2 waveOffset;
+    [SerializeField] Vector2 waveStep = new Vector2(0.01f, 0.01f);
 
-    [SerializeField]
-    private float edgeWidth = 1f; // widht of our road barrier at the edge of our road
+    [Header("Materials"),Space]
 
-    [SerializeField]
-    private float edgeHeight = 1f;
+    [SerializeField] List<Material> materialsList = new List<Material>();
+    [SerializeField] GameObject car;
+    [SerializeField] GameObject checkPoint;
+    [SerializeField] string nextScene;
 
-    [SerializeField]
-    private int submeshSize = 6;
+    public int checkpoints = 0;
+    private int randomStart;
+    private int mark;
 
-    [SerializeField]
-    private float wavyness = 5f;
+    List<Vector3> points;
 
-    [SerializeField]
-    private float waveScale = 0.1f;
+    private void Start()
+    {
+        GenerateRoad();
+        SpawnCar();
+    }
+    private void Update()
+    {
+        NextLevel();
+    }
 
-    [SerializeField]
-    private Vector2 waveOffset;
-
-    [SerializeField]
-    private Vector2 waveStep = new Vector2(0.01f, 0.01f);
-
-    [SerializeField]
-    private bool stripeCheck = true;
-
-    [SerializeField]
-    private GameObject car;
-
-    void Start()
+    void GenerateRoad()
     {
         MeshFilter meshFilter = this.GetComponent<MeshFilter>();
-
         MeshCollider meshCollider = this.GetComponent<MeshCollider>();
+        MeshBuilder meshBuilder = new MeshBuilder(materialsList.Count);
 
-        MeshBuilder meshBuilder = new MeshBuilder(submeshSize);
-
-
-        //1. Divide the circular race track into segments denoted in degrees and each point is defined by each segment
-        //   Create the points and store them in a list
         float segmentDegrees = 360f / segments;
-
-        List<Vector3> points = new List<Vector3>();
+        points = new List<Vector3>();
 
         for (float degrees = 0; degrees < 360f; degrees += segmentDegrees)
         {
@@ -67,7 +61,7 @@ public class RoadMaker : MonoBehaviour
             points.Add(point);
         }
 
-        Vector2 wave = this.waveOffset;
+        Vector2 wave = RandomWaves();
 
         for (int i = 0; i < points.Count; i++)
         {
@@ -84,7 +78,6 @@ public class RoadMaker : MonoBehaviour
             points[i] += centreDirection * noise * control;
         }
 
-        //2. function to define the path - the path is defined by each segment
         for (int i = 1; i < points.Count + 1; i++)
         {
             Vector3 pPrev = points[i - 1];
@@ -94,68 +87,47 @@ public class RoadMaker : MonoBehaviour
             ExtrudeRoad(meshBuilder, pPrev, pCurr, pNext);
         }
 
-        car.transform.position = points[0];
-        car.transform.LookAt(points[1]);
-
         meshFilter.mesh = meshBuilder.CreateMesh();
-
+        MeshRenderer meshRenderer = this.GetComponent<MeshRenderer>();
+        meshRenderer.materials = materialsList.ToArray();
         meshCollider.sharedMesh = meshFilter.mesh;
     }
 
-    //3. This method will used to create the different segments for each segment we are going to draw the road marker 
-    //   (i.e. white line in the middle), draw the road on each side of the line, draw the edges - all these are going 
-    //   to be placed in different positions
     private void ExtrudeRoad(MeshBuilder meshBuilder, Vector3 pPrev, Vector3 pCurr, Vector3 pNext)
     {
-        //Road Line
         Vector3 offset = Vector3.zero;
         Vector3 targetOffset = Vector3.forward * lineWidth;
 
         MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, 0);
 
-        //Road
         offset += targetOffset;
         targetOffset = Vector3.forward * roadWidth;
 
         MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, 1);
 
-        int stripeSubmesh = 2;
-
-        if (stripeCheck)
-        {
-            stripeSubmesh = 3;
-        }
-
-        stripeCheck = !stripeCheck;
-
-        //Edge
         offset += targetOffset;
         targetOffset = Vector3.up * edgeHeight;
 
-        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, stripeSubmesh);
+        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, 2);
 
-        //Edge Top
         offset += targetOffset;
         targetOffset = Vector3.forward * edgeWidth;
 
-        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, stripeSubmesh);
+        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, 2);
 
-        //Edge
         offset += targetOffset;
         targetOffset = -Vector3.up * edgeHeight;
 
-        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, stripeSubmesh);
+        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, 2);
 
     }
 
-    //4. Create each quad
-    private void MakeRoadQuad(MeshBuilder meshBuilder, Vector3 pPrev, Vector3 pCurr, Vector3 pNext,
-                              Vector3 offset, Vector3 targetOffset, int submesh)
+    private void MakeRoadQuad(MeshBuilder meshBuilder, Vector3 pPrev, Vector3 pCurr, Vector3 pNext, 
+        Vector3 offset, Vector3 targetOffset, int submesh)
     {
         Vector3 forward = (pNext - pCurr).normalized;
         Vector3 forwardPrev = (pCurr - pPrev).normalized;
 
-        //Build Outer Track
         Quaternion perp = Quaternion.LookRotation(Vector3.Cross(forward, Vector3.up));
         Quaternion perpPrev = Quaternion.LookRotation(Vector3.Cross(forwardPrev, Vector3.up));
 
@@ -168,7 +140,6 @@ public class RoadMaker : MonoBehaviour
         meshBuilder.BuildTriangle(topLeft, topRight, bottomLeft, submesh);
         meshBuilder.BuildTriangle(topRight,bottomRight,bottomLeft, submesh);
 
-        //Build Inner Track
         perp = Quaternion.LookRotation(Vector3.Cross(-forward, Vector3.up));
         perpPrev = Quaternion.LookRotation(Vector3.Cross(-forwardPrev, Vector3.up));
 
@@ -180,5 +151,57 @@ public class RoadMaker : MonoBehaviour
 
         meshBuilder.BuildTriangle(bottomLeft, bottomRight, topLeft, submesh);
         meshBuilder.BuildTriangle(bottomRight, topRight, topLeft, submesh);
+    }
+
+    Vector2 RandomWaves()
+    {
+        waveOffset.x = Random.Range(-100, 100);
+        waveOffset.y = Random.Range(-100, 100);
+        return waveOffset;
+    }
+
+    void SpawnCar()
+    {
+        GameObject tempCar = Instantiate(car);
+        randomStart = Random.Range(0, points.Count);
+
+        tempCar.transform.parent = transform;
+        tempCar.transform.position = points[randomStart] + (Vector3.up * 2);
+
+        if (randomStart == points.Count - 1)
+        {
+            tempCar.transform.LookAt(points[0] + (Vector3.up * 2));
+        }
+        else
+        {
+            tempCar.transform.LookAt(points[randomStart + 1] + (Vector3.up * 2));
+        }
+        SpawnCheckPoint();
+    }
+
+    public void SpawnCheckPoint()
+    {
+        mark = randomStart + (80 * (checkpoints + 1));
+        GameObject check = Instantiate(checkPoint);
+
+        if(mark > points.Count - 1)
+        {
+            mark -= points.Count;
+        }
+
+        print("Random point: " + randomStart + " Location: " + points[randomStart] + "\nMark: " + mark + " Location: " + points[mark]);
+
+        check.transform.position = points[mark] + (Vector3.up * 5);
+        check.transform.LookAt(points[mark + 1] + (Vector3.up * 5));
+        check.transform.parent = transform;
+
+    }
+
+    void NextLevel()
+    {
+        if (checkpoints > 3)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
     }
 }
